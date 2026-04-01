eb29b1174fd2f2b06c4c7f6743a61d56f6850f1d
Umang Vaghamshi <trionn.umang@gmail.com>
2026-04-01 15:57:44 +0530

    Fixed banner canvas hook

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
+import type { Metadata } from "next";
 import PagesWrapper from "@/components/PagesWrapper";
 import SmoothScrolling from "@/components/SmoothScrolling";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "TRIONN | Crafting Award-Winning Digital Journeys.",
+  description:
+    "Trionn® - Redefining Digital Creativity: Your Partner for UI/UX, Mobile App, Web App, Branding, Creative Website Design, and Custom Development in Rajkot, Gujarat, India.",
+};
 
 export default function RootLayout({
   children,
--- components/Sections/Home/Vision.tsx
+++ components/Sections/Home/Vision.tsx
@@ -4,111 +4,188 @@ import Marquee from "@/components/Marquee";
 
 export default function Vision() {
   return (
-    <section className="min-h-screen text-light-font relative flex flex-col items-center justify-center py-37.5 pb-20 w-full mix-blend-difference">
+    <section className=" ">
       <div
-        className="tr__container w-full grid grid-cols-12 grid-rows-1 gap-x-6"
         id="s3-text"
+        className="w-screen h-screen relative z-20 flex flex-col justify-center bg-transparent text-left  overflow-hidden items-center py-37.5 pb-20 mix-blend-difference text-light-font"
       >
-        <BlurTextReveal
-          as="span"
-          html={`Focused vision. <br/>Measured execution.`}
-          animationType="chars"
-          stagger={0.05}
-          className="title z-3 col-start-2 col-span-11"
-        />
-      </div>
-      <div
-        className="relative z-3 flex-col flex justify-center items-start pointer-events-none pt-40 pb-20"
-        id="s3-marquee"
-      >
-        <Marquee gap={0} speed={0.8}>
-          <div className="uppercase mrquee-text flex items-center">
-            <span className="marquee-text-item">Inspire</span>
-            <svg
-              width="40"
-              height="40"
-              viewBox="0 0 40 40"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="mx-[4.582vw] w-[2.291vw] h-[2.291vw]"
+        <div className="tr__container w-full grid grid-cols-12 grid-rows-1 gap-x-6">
+          <BlurTextReveal
+            as="span"
+            html={`Focused vision. <br/>Measured execution.`}
+            animationType="chars"
+            stagger={0.05}
+            className="title z-3 col-start-2 col-span-11"
+          />
+        </div>
+        <div
+          className="relative z-3 flex-col flex justify-center items-start pointer-events-none pt-40 pb-20"
+          id="s3-marquee"
+        >
+          <div className="w-full overflow-hidden">
+            <div
+              id="marquee-track"
+              className="flex items-center gap-0 whitespace-nowrap will-change-transform mrquee-text uppercase"
             >
-              <line
-                x1="20.2256"
-                y1="-2.18557e-08"
-                x2="20.2256"
-                y2="40"
-                stroke="#D8D8D8"
-              />
-              <line
-                x1="40"
-                y1="20.226"
-                x2="-4.37114e-08"
-                y2="20.226"
-                stroke="#D8D8D8"
-              />
-            </svg>
-            <span className="marquee-text-item">innovate</span>
-            <svg
-              width="40"
-              height="40"
-              viewBox="0 0 40 40"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="mx-[4.582vw] w-[2.291vw] h-[2.291vw]"
-            >
-              <line
-                x1="20.2256"
-                y1="-2.18557e-08"
-                x2="20.2256"
-                y2="40"
-                stroke="#D8D8D8"
-              />
-              <line
-                x1="40"
-                y1="20.226"
-                x2="-4.37114e-08"
-                y2="20.226"
-                stroke="#D8D8D8"
-              />
-            </svg>
-            <span className="marquee-text-item">Impact</span>
-            <svg
-              width="40"
-              height="40"
-              viewBox="0 0 40 40"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="mx-[4.582vw] w-[2.291vw] h-[2.291vw]"
-            >
-              <line
-                x1="20.2256"
-                y1="-2.18557e-08"
-                x2="20.2256"
-                y2="40"
-                stroke="#D8D8D8"
-              />
-              <line
-                x1="40"
-                y1="20.226"
-                x2="-4.37114e-08"
-                y2="20.226"
-                stroke="#D8D8D8"
-              />
-            </svg>
+              <span className="marquee-text-item">Inspire</span>
+              <svg
+                width="40"
+                height="40"
+                viewBox="0 0 40 40"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="mx-[4.582vw] w-[2.291vw] h-[2.291vw]"
+              >
+                <line
+                  x1="20.2256"
+                  y1="-2.18557e-08"
+                  x2="20.2256"
+                  y2="40"
+                  stroke="#D8D8D8"
+                />
+                <line
+                  x1="40"
+                  y1="20.226"
+                  x2="-4.37114e-08"
+                  y2="20.226"
+                  stroke="#D8D8D8"
+                />
+              </svg>
+              <span className="marquee-text-item">Innovate</span>
+              <svg
+                width="40"
+                height="40"
+                viewBox="0 0 40 40"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="mx-[4.582vw] w-[2.291vw] h-[2.291vw]"
+              >
+                <line
+                  x1="20.2256"
+                  y1="-2.18557e-08"
+                  x2="20.2256"
+                  y2="40"
+                  stroke="#D8D8D8"
+                />
+                <line
+                  x1="40"
+                  y1="20.226"
+                  x2="-4.37114e-08"
+                  y2="20.226"
+                  stroke="#D8D8D8"
+                />
+              </svg>
+              <span className="marquee-text-item">Impact</span>
+              <svg
+                width="40"
+                height="40"
+                viewBox="0 0 40 40"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="mx-[4.582vw] w-[2.291vw] h-[2.291vw]"
+              >
+                <line
+                  x1="20.2256"
+                  y1="-2.18557e-08"
+                  x2="20.2256"
+                  y2="40"
+                  stroke="#D8D8D8"
+                />
+                <line
+                  x1="40"
+                  y1="20.226"
+                  x2="-4.37114e-08"
+                  y2="20.226"
+                  stroke="#D8D8D8"
+                />
+              </svg>
+              <span className="marquee-text-item">Inspire</span>
+              <svg
+                width="40"
+                height="40"
+                viewBox="0 0 40 40"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="mx-[4.582vw] w-[2.291vw] h-[2.291vw]"
+              >
+                <line
+                  x1="20.2256"
+                  y1="-2.18557e-08"
+                  x2="20.2256"
+                  y2="40"
+                  stroke="#D8D8D8"
+                />
+                <line
+                  x1="40"
+                  y1="20.226"
+                  x2="-4.37114e-08"
+                  y2="20.226"
+                  stroke="#D8D8D8"
+                />
+              </svg>
+              <span className="marquee-text-item">Innovate</span>
+              <svg
+                width="40"
+                height="40"
+                viewBox="0 0 40 40"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="mx-[4.582vw] w-[2.291vw] h-[2.291vw]"
+              >
+                <line
+                  x1="20.2256"
+                  y1="-2.18557e-08"
+                  x2="20.2256"
+                  y2="40"
+                  stroke="#D8D8D8"
+                />
+                <line
+                  x1="40"
+                  y1="20.226"
+                  x2="-4.37114e-08"
+                  y2="20.226"
+                  stroke="#D8D8D8"
+                />
+              </svg>
+              <span className="marquee-text-item">Impact</span>
+              <svg
+                width="40"
+                height="40"
+                viewBox="0 0 40 40"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="mx-[4.582vw] w-[2.291vw] h-[2.291vw]"
+              >
+                <line
+                  x1="20.2256"
+                  y1="-2.18557e-08"
+                  x2="20.2256"
+                  y2="40"
+                  stroke="#D8D8D8"
+                />
+                <line
+                  x1="40"
+                  y1="20.226"
+                  x2="-4.37114e-08"
+                  y2="20.226"
+                  stroke="#D8D8D8"
+                />
+              </svg>
+            </div>
           </div>
-        </Marquee>
-      </div>
-      <div
-        className="tr__container w-full grid grid-cols-12 grid-rows-1 gap-x-6 pb-10"
-        id="s3-bottom-text"
-      >
-        <BlurTextReveal
-          as="span"
-          html={`✦ From idea to outcome.`}
-          animationType="chars"
-          stagger={0.05}
-          className="title z-3 col-span-12 text-center"
-        />
+        </div>
+        <div
+          className="tr__container w-full grid grid-cols-12 grid-rows-1 gap-x-6 pb-10"
+          id="s3-bottom-text"
+        >
+          <BlurTextReveal
+            as="span"
+            html={`✦ From idea to outcome.`}
+            animationType="chars"
+            stagger={0.05}
+            className="title z-3 col-span-12 text-center"
+          />
+        </div>
       </div>
     </section>
   );
