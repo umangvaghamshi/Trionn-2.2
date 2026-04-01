6a2e586648bdba5c75e184c9495529bbe41b6f54
Umang Vaghamshi <trionn.umang@gmail.com>
2026-04-01 16:04:02 +0530

    Update banner marquee

--- components/Sections/Home/Vision.tsx
+++ components/Sections/Home/Vision.tsx
@@ -18,15 +18,9 @@ export default function Vision() {
             className="title z-3 col-start-2 col-span-11"
           />
         </div>
-        <div
-          className="relative z-3 flex-col flex justify-center items-start pointer-events-none pt-40 pb-20"
-          id="s3-marquee"
-        >
-          <div className="w-full overflow-hidden">
-            <div
-              id="marquee-track"
-              className="flex items-center gap-0 whitespace-nowrap will-change-transform mrquee-text uppercase"
-            >
+        <div className="relative z-3 flex-col flex justify-center items-start pointer-events-none pt-40 pb-20">
+          <Marquee gap={0} speed={0.8}>
+            <div className="uppercase mrquee-text flex items-center">
               <span className="marquee-text-item">Inspire</span>
               <svg
                 width="40"
@@ -51,79 +45,7 @@ export default function Vision() {
                   stroke="#D8D8D8"
                 />
               </svg>
-              <span className="marquee-text-item">Innovate</span>
-              <svg
-                width="40"
-                height="40"
-                viewBox="0 0 40 40"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="mx-[4.582vw] w-[2.291vw] h-[2.291vw]"
-              >
-                <line
-                  x1="20.2256"
-                  y1="-2.18557e-08"
-                  x2="20.2256"
-                  y2="40"
-                  stroke="#D8D8D8"
-                />
-                <line
-                  x1="40"
-                  y1="20.226"
-                  x2="-4.37114e-08"
-                  y2="20.226"
-                  stroke="#D8D8D8"
-                />
-              </svg>
-              <span className="marquee-text-item">Impact</span>
-              <svg
-                width="40"
-                height="40"
-                viewBox="0 0 40 40"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="mx-[4.582vw] w-[2.291vw] h-[2.291vw]"
-              >
-                <line
-                  x1="20.2256"
-                  y1="-2.18557e-08"
-                  x2="20.2256"
-                  y2="40"
-                  stroke="#D8D8D8"
-                />
-                <line
-                  x1="40"
-                  y1="20.226"
-                  x2="-4.37114e-08"
-                  y2="20.226"
-                  stroke="#D8D8D8"
-                />
-              </svg>
-              <span className="marquee-text-item">Inspire</span>
-              <svg
-                width="40"
-                height="40"
-                viewBox="0 0 40 40"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="mx-[4.582vw] w-[2.291vw] h-[2.291vw]"
-              >
-                <line
-                  x1="20.2256"
-                  y1="-2.18557e-08"
-                  x2="20.2256"
-                  y2="40"
-                  stroke="#D8D8D8"
-                />
-                <line
-                  x1="40"
-                  y1="20.226"
-                  x2="-4.37114e-08"
-                  y2="20.226"
-                  stroke="#D8D8D8"
-                />
-              </svg>
-              <span className="marquee-text-item">Innovate</span>
+              <span className="marquee-text-item">innovate</span>
               <svg
                 width="40"
                 height="40"
@@ -172,12 +94,9 @@ export default function Vision() {
                 />
               </svg>
             </div>
-          </div>
+          </Marquee>
         </div>
-        <div
-          className="tr__container w-full grid grid-cols-12 grid-rows-1 gap-x-6 pb-10"
-          id="s3-bottom-text"
-        >
+        <div className="tr__container w-full grid grid-cols-12 grid-rows-1 gap-x-6 pb-10">
           <BlurTextReveal
             as="span"
             html={`✦ From idea to outcome.`}
