9351d47caf47ddbb7a058e8c49cbfc2db56a4b52
Umang Vaghamshi <trionn.umang@gmail.com>
2026-04-21 16:17:16 +0530

    About page intro block structure

---
+++ components/Sections/About/AboutIntro.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { BlurTextReveal } from "@/components/TextAnimation";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import Image from "next/image";
+import { useCallback, useRef, useState } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import LinePlus from "@/components/LinePlus";
+import { WordShiftButton } from "@/components/Button";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function AboutIntro() {
+  return (
+    <section className="relative w-full pb-37.5 bg-white z-3 overflow-hidden text-dark-font">
+      <div className="tr__container  flex flex-col justify-between">
+        <div className="title-block grid grid-cols-12 gap-x-6">
+          <div className="col-span-11 col-start-2 mb-10">
+            <BlurTextReveal
+              as="span"
+              html={`At Trionn,`}
+              animationType="chars"
+              stagger={0.05}
+              className="title block mb-4"
+            />
+            <h3 className="max-w-167.5 w-full">
+              We build teams around ideas. Each project is led by designers,
+              engineers, and specialists chosen specifically for the challenge
+              at hand.
+            </h3>
+          </div>
+        </div>
+        <div className="grid grid-cols-12 gap-x-6 my-25">
+          <div className="col-span-11 col-start-2">
+            <LinePlus
+              lineClass={"opacity-15 bg-grey-line"}
+              plusClass={"col-start-9"}
+              iconColor={"#272727"}
+            />
+          </div>
+        </div>
+        <div className="grid grid-cols-12 gap-x-6">
+          <div className="col-span-11 col-start-2 grid grid-cols-12 gap-x-6 grid-rows-1">
+            <div className="left-block grid grid-cols-3 gap-x-6 col-span-3 align-top">
+              <div className="col-span-1">
+                <BlurTextReveal
+                  as="span"
+                  html={`Est. 2012 —`}
+                  animationType="chars"
+                  stagger={0.05}
+                  className="title block bg-cream py-1.5 px-2 rounded-sm"
+                />
+              </div>
+              <BlurTextReveal
+                as="span"
+                html={`13+ years <br/>shaping digital <br/>direction.`}
+                animationType="chars"
+                stagger={0.02}
+                className="title block col-span-2"
+              />
+            </div>
+            <div className="left-block flex flex-col col-span-3 col-start-9">
+              <p className="mb-18 max-w-98">
+                We&apos;ve grown through experimentation, learning, and
+                refinement — shaping a practice focused on clarity, craft, and
+                long-term impact.
+                <br />
+                <br />
+                Today, we partner with startups and global brands to design
+                digital products, platforms, and systems that scale with purpose
+                — and endure.
+              </p>
+              <WordShiftButton text="let's connect" href="#" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
