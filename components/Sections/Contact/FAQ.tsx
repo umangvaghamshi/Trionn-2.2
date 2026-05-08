45c978fdb1053cbecf79f2569fe5f1ac79cbf9ca
Umang Vaghamshi <trionn.umang@gmail.com>
2026-05-08 15:53:41 +0530

    start contact page responsive

--- components/Sections/Contact/FAQ.tsx
+++ components/Sections/Contact/FAQ.tsx
@@ -4,20 +4,20 @@ import { contactFAQ } from "@/data";
 
 export default function FAQ() {
   return (
-    <section className="py-37.5 relative bg-[linear-gradient(0deg,#C3C3C3_0%,#FFFFFF_100%)] text-dark-font">
-      <div className="tr__container grid grid-cols-12 gap-6">
-        <div className="col-span-4 col-start-2 flex flex-col gap-10">
+    <section className="py-20 lg:py-37.5 relative bg-[linear-gradient(0deg,#C3C3C3_0%,#FFFFFF_100%)] text-dark-font">
+      <div className="tr__container grid grid-cols-12 md:gap-6">
+        <div className="col-span-12 md:col-span-6 lg:col-span-4 lg:col-start-2 flex flex-col gap-4 md:gap-10">
           <BlurTextReveal
             as="h2"
             html="Questions"
             animationType="chars"
             stagger={0.08}
           />
-          <p className="small max-w-42">
+          <p className="small lg:max-w-42">
             Common things people ask before we begin.
           </p>
         </div>
-        <div className="col-span-5 col-start-7">
+        <div className="col-span-12 md:col-span-6 lg:col-span-5 md:col-start-7">
           <Accordion items={contactFAQ} />
         </div>
       </div>
