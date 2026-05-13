2dcb8663e0749699b8df818bd0759c3acb7b67c4
Umang Vaghamshi <trionn.umang@gmail.com>
2026-05-13 12:43:17 +0530

    About hero section responsive

--- components/Sections/About/AboutHero.tsx
+++ components/Sections/About/AboutHero.tsx
@@ -143,7 +143,7 @@ export default function AboutHero() {
             />
           </span>
         </div>
-        <div className="tr__container grid grid-cols-12 gap-x mb-25 absolute top-[65%] md:top-[46%] lg:top-[50%] w-full z-5 translate-y-full pointer-events-none">
+        <div className="tr__container grid grid-cols-12 absolute top-[66%] sm:top-[76%] md:top-[50%] w-full z-5 translate-y-full pointer-events-none">
           <BlurTextReveal
             as="span"
             text={`We design and build digital experiences that scale, perform, and endure.`}
